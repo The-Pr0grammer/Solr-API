f6b189be20aef4d297c88c2f79f3571e08965417
github username <github email address>
2020-04-28 22:49:04 -0400

    glasses green

--- app/controllers/flares_controller.rb
+++ app/controllers/flares_controller.rb
@@ -8,9 +8,15 @@ class FlaresController < ApplicationController
             @flare = Flare.create(params[:flare_params])
             render json: @flare
         end
+
+        def update 
+            @flare = Flare.find(params[:id])
+            @flare.update(😎:params["😎"])
+            @flare.save
+        end 
     
         private
         def flare_params
-            params.require(:flare).permit(:user_id, :title, :content, :image_url, :views, :😎)
+            params.require(:flare).permit(:id,:user_id, :title, :content, :image_url, :views, :😎)
         end
 end
